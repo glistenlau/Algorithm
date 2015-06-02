44d8769265af06905bb23c9a5ab23c22fdd75e0d
Yi Liu <purplesky8985@gmail.com>
2015-06-02 11:36:37 -0500

    optimize DP algorithm

--- DP/ChessMetric.java
+++ DP/ChessMetric.java
@@ -1,51 +1,35 @@
-import java.util.ArrayDeque;
-import java.util.Queue;
-import java.util.Stack;
-
 /**
  * Created by YiLIU on 6/1/15.
  */
 public class ChessMetric {
   public long howMany(int size, int[] start, int[] end, int numMove) {
-    Queue<int[]> orderStep = new ArrayDeque<>();
-    orderStep.add(start);
-    int[] possibleStep = new int[numMove + 1];
-    long[][] maxStep = new long[size][size];
-    possibleStep[0] = 1;
+    final int[] stepX = {-1, -1, -1, 0, 0, 1, 1, 1, -2, -2, 2, 2, -1, 1, -1, 1};
+    final int[] stepY = {-1, 0, 1, -1, 1, -1, 0, 1, -1, 1, -1, 1, -2, -2, 2, 2};
 
-    for (int n = 1; n <= numMove;) {
-      if (possibleStep[n - 1] == 0) {
-        ++n;
-        continue;
-      }
-      int[] current = orderStep.remove();
-      possibleStep[n - 1]--;
-      int x = current[0];
-      int y = current[1];
+    long[][][] maxStep = new long[size][size][numMove + 1];
+    maxStep[start[0]][start[1]][0] = 1;
 
-      for (int i = -2; i <= 2; ++i) {
-        for (int j = -2; j <= 2; ++j) {
-          if ((i == -2 || i == 2) && (j != -1 && j != 1) ||
-              (j == -2 || j == 2) && (i != -1 && i != 1))
-            continue;
-          if (i == 0 && j == 0) continue;
-          if (x + i >= 0 && y + j >= 0 && x + i <= size - 1 && y + j <= size - 1) {
-            int[] valid = {x + i, y + j};
-            maxStep[x + i][y + j]++;
-            orderStep.add(valid);
-            possibleStep[n]++;
+    for (int n = 1; n <= numMove; ++n) {
+      for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < size; ++j) {
+          for (int k = 0; k < stepX.length; ++k) {
+            int nX = i + stepX[k];
+            int nY = j + stepY[k];
+            if (nX >=0 && nX < size && nY >= 0 && nY < size) {
+              maxStep[nX][nY][n] += maxStep[i][j][n - 1];
+            }
           }
         }
       }
     }
-    return maxStep[end[0]][end[1]];
-  }
+    return maxStep[end[0]][end[1]][numMove];
+ }
 
   public static void main(String[] args) {
-    int size = 100;
-    int[] start = {0, 0};
-    int[] end = {0, 99};
-    int numMove = 50;
+    int size = 8;
+    int[] start = {2, 3};
+    int[] end = {7, 7};
+    int numMove = 9;
     System.out.println(new ChessMetric().howMany(size, start, end, numMove));
   }
 }

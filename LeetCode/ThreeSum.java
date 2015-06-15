8ff0035f2d2fc0811577564c771a7f7e063c7eeb
Yi Liu <purplesky8985@gmail.com>
2015-06-14 21:06:38 -0500

    Finished Leetcode Problem ThreeSum

--- LeetCode/ThreeSum.java
+++ LeetCode/ThreeSum.java
@@ -9,9 +9,12 @@ public class ThreeSum {
   private List<List<Integer>> result = new ArrayList<>();
 
   public List<List<Integer>> threeSumR(int[] nums) {
+    long startTime = System.currentTimeMillis();
     Arrays.sort(nums);
     List<Integer> taken = new ArrayList<>();
     threeSumHelper(nums, 0, taken, 0);
+    long endTime = System.currentTimeMillis();
+    System.out.println("Run Time: " + (endTime - startTime) + " ms");
     return result;
   }
 
@@ -40,6 +43,7 @@ public class ThreeSum {
   }
 
   public List<List<Integer>> threeSum(int[] nums) {
+    long startTime = System.currentTimeMillis();
     List<List<Integer>> result = new ArrayList<>();
     if (nums == null || nums.length < 3) {
       return result;
@@ -75,11 +79,14 @@ public class ThreeSum {
         }
       }
     }
+    long endTime = System.currentTimeMillis();
+    System.out.println("Run Time: " + (endTime - startTime) + " ms");
     return result;
   }
 
   public static void main(String[] args) {
     int[] test = {7,-1,14,-12,-8,7,2,-15,8,8,-8,-14,-4,-5,7,9,11,-4,-15,-6,1,-14,4,3,10,-5,2,1,6,11,2,-2,-5,-7,-6,2,-15,11,-6,8,-4,2,1,-1,4,-6,-15,1,5,-15,10,14,9,-8,-6,4,-6,11,12,-15,7,-1,-9,9,-1,0,-4,-1,-12,-2,14,-9,7,0,-3,-4,1,-2,12,14,-10,0,5,14,-1,14,3,8,10,-8,8,-5,-2,6,-11,12,13,-7,-12,8,6,-13,14,-2,-5,-11,1,3,-6};
     System.out.println(new ThreeSum().threeSum(test));
+    System.out.println(new ThreeSum().threeSumR(test));
   }
 }

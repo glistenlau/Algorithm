56e4a06a07e4b22184c007a9a7822de45b708b59
Yi Liu <purplesky8985@gmail.com>
2015-07-03 21:01:38 -0500

    Finished LintCode problem Remove Duplicates from Sorted List II

--- LintCode/RemoveDuplicatedFromSortedListII.java
+++ LintCode/RemoveDuplicatedFromSortedListII.java
@@ -1,18 +1,28 @@
 /**
- * Definition for ListNode.
+ * Remove Duplicates from Sorted List II
+ * http://www.lintcode.com/en/problem/remove-duplicates-from-sorted-list-ii/
+ * Given a sorted linked list, delete all nodes that have duplicate numbers,
+ * leaving only distinct numbers form the original list.
+ *
+ * Example
+ * Given 1->2->3->3->4->4->5, return 1->2->5.
+ * Given 1->1->1->2->3, return 2->3.
+ */
+/**
+ * Definition for ListNode
  * public class ListNode {
  *     int val;
  *     ListNode next;
- *     ListNode(int val) {
- *         this.val = val;
- *         this.next = null;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
  *     }
  * }
  */
-public class temp {
+public class Solution {
   /**
-   * @param head: The head of linked list.
-   * @return: The new head of reversed linked list.
+   * @param ListNode head is the head of the linked list
+   * @return: ListNode head of the linked list
    */
   public static ListNode deleteDuplicates(ListNode head) {
     // write your code here
@@ -31,17 +41,8 @@ public class temp {
         head = head.next;
       }
     }
+    cur.next = null;
     return dummy.next;
-  }
-
-  public static void main(String[] args) {
-    ListNode test = new ListNode(0);
-    test.next = new ListNode(1);
-    test.next.next = new ListNode(2);
-    test.next.next.next = new ListNode(2);
-    test.next.next.next.next = new ListNode(2);
-    ListNode result = new temp().deleteDuplicates(test);
-    return;
   }
 }
 

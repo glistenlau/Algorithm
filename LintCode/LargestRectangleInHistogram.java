4b0dec9f03db6625a5c147d4af9eb7cd9f0e8869
Yi Liu <purplesky8985@gmail.com>
2015-07-12 16:00:21 -0500

    Finished LintCode problem Largest Rectangle in Histogram

---
+++ LintCode/LargestRectangleInHistogram.java
@@ -0,0 +1,33 @@
+/**
+ * Largest Rectangle in Histogram
+ * www.lintcode.com/en/problem/largest-rectangle-in-histogram/
+ *
+ * Given n non-negative integers representing the histogram's bar height
+ * where the width of each bar is 1, find the area of largest rectangle in
+ * the histogram.
+ */
+
+public class Solution {
+  /**
+   * @param height: A list of integer
+   * @return: The area of largest rectangle in the histogram
+   */
+  public int largestRectangleArea(int[] height) {
+    if (height == null || height.length == 0) {
+      return 0;
+    }
+    Stack<Integer> myStack = new Stack<Integer>();
+    int max = 0;
+    for (int i = 0; i <= height.length; i++) {
+      int curt = i == height.length ? -1: height[i];
+      while(!myStack.isEmpty() && curt <= height[myStack.peek()]) {
+        int h = height[myStack.pop()];
+        int w = myStack.isEmpty() ? i: i - 1 - myStack.peek();
+        max = Math.max(max, h * w);
+      }
+      myStack.push(i);
+    }
+    return max;
+  }
+}
+

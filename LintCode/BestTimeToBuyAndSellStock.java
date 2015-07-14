72dfa8ff2d26ae8d3894f5061f8a1bf6d75e4d4d
Yi Liu <purplesky8985@gmail.com>
2015-07-14 08:40:51 -0500

    Finished LintCode problem Best Time to Buy and Sell stock

---
+++ LintCode/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,34 @@
+/**
+ * Best Time to Buy and Sell Stock
+ * www.lintcode.com/en/problem/best-time-to-buy-and-sell-stock/
+ *
+ * Say you have an array for which the ith element is the price of a given
+ * stock on day i.
+ *
+ * If you were only permitted to complete at most one transaction(ie, buy one
+ * and sell one share of the stock), design an algorithm to find the maximum
+ * profit.
+ *
+ * Example
+ * Given an array[3, 2, 3, 1, 2], return 1.
+ */
+
+public class Solution {
+  /**
+   * @param prices: Given an integer array
+   * @return: Maximum profit
+   */
+  public int maxProfit(int[] prices) {
+    if (prices == null || prices.length == 0) {
+      return 0;
+    }
+
+    int preMin = prices[0];
+    int result = 0;
+    for (int i = 1; i < prices.length; i++) {
+      result = Math.max(result, prices[i] - preMin);
+      preMin = Math.min(preMin, prices[i]);
+    }
+    return result;
+  }
+}
